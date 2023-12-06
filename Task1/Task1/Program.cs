d565edcbc112a6288c7d2103d22fe8bf256bfe51
LinKate7 <123402287+LinKate7@users.noreply.github.com>
2023-12-06 17:45:19 +0200

    Add Task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿//Single Responsibility Principle, ось цей solid порушено 
+class Item
+{
+
+}
+
+interface IOrder
+{
+    public void CalculateTotalSum();
+    public void GetItems();
+    public void GetItemCount();
+    public void AddItem(Item item);
+    public void DeleteItem(Item item);
+}
+interface IShow
+{
+    public void ShowOrder();
+}
+
+interface IRepository
+{
+    public void Load();
+    public void Save();
+    public void Update();
+    public void Delete();
+}
+
+interface IPrint
+{
+    public void PrintOrder();
+}
+
+class Order : IOrder
+{
+    private List<Item> itemList;
+    internal List<Item> ItemList
+    {
+        get
+        {
+            return itemList;
+        }
+        set
+        {
+            itemList = value;
+        }
+    }
+    public void CalculateTotalSum() {/*...*/}
+    public void GetItems() {/*...*/}
+    public void GetItemCount() {/*...*/}
+    public void AddItem(Item item) {/*...*/}
+    public void DeleteItem(Item item) {/*...*/}
+}
+
+class Repository : IRepository
+{
+    public void Load() {/*...*/}
+    public void Save() {/*...*/}
+    public void Update() {/*...*/}
+    public void Delete() {/*...*/}
+}
+
+class OrderPrint : IPrint
+{
+    public void PrintOrder() {/*...*/}
+}
+
+class OrderShow : IShow
+{
+    public void ShowOrder() {/*...*/}
+}
+
