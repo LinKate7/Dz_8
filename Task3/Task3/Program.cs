bee3f4e251513def55432ad1b4f4d9e4eb674043
LinKate7 <123402287+LinKate7@users.noreply.github.com>
2023-12-06 17:47:25 +0200

    Add Task3

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Liskov Substitution Principle ось цей solid порушено
+class Program
+{
+    static void Main(string[] args)
+    {
+        Square square = new Square();
+        square.Side = 10;
+        square.GetArea();
+        Console.WriteLine(square.GetArea());
+        Console.ReadKey();
+    }
+}
+interface IArea
+{
+    int GetArea();
+}
+class Rectangle : IArea
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int GetArea()
+    {
+        return Width * Height;
+    }
+}
+class Square : IArea
+{
+    public int Side { get; set; }
+
+    public int GetArea()
+    {
+        return Side * Side;
+    }
+}
